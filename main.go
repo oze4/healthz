48744418044e87dbd10a18b62b6e79b1d6ff1f80
Matt Oestreich <matthewpoestreich@gmail.com>
2020-10-03 19:27:05 -0500

    init

--- main.go
+++ main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
-    "errors"
-    "fmt"
 
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/rest"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
-    client, err := newInClusterClient()
-    if err != nil {
-        panic(err.Error())
-    }
+	client, err := newInClusterClient()
+	if err != nil {
+		panic(err.Error())
+	}
 
-    path := "/healthz"
-    content, err := client.Discovery().RESTClient().Get().AbsPath(path).DoRaw()
-    if err != nil {
-        fmt.Printf("ErrorBadRequst : %s\n", err.Error())
-        os.Exit(1)
-    }
+	path := "/healthz"
+	content, err := client.Discovery().RESTClient().Get().AbsPath(path).DoRaw()
+	if err != nil {
+		fmt.Printf("ErrorBadRequst : %s\n", err.Error())
+		os.Exit(1)
+	}
 
-    contentStr := string(content)
-    if contentStr != "ok" {
-        fmt.Printf("ErrorNotOk : response != 'ok' : %s\n", contentStr)
-        os.Exit(1)
-    }
+	contentStr := string(content)
+	if contentStr != "ok" {
+		fmt.Printf("ErrorNotOk : response != 'ok' : %s\n", contentStr)
+		os.Exit(1)
+	}
 
-    fmt.Printf("Success : ok!")
-    os.Exit(0)
+	fmt.Printf("Success : ok!")
+	os.Exit(0)
 }
 
 func newInClusterClient() (*kubernetes.Clientset, error) {
-    config, err := rest.InClusterConfig()
-    if err != nil {
-        return &kubernetes.Clientset{}, errors.New("Failed loading client config")
-    }
-    clientset, err := kubernetes.NewForConfig(config)
-    if err != nil {
-        return &kubernetes.Clientset{}, errors.New("Failed getting clientset")
-    }
-    return clientset, nil
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return &kubernetes.Clientset{}, errors.New("Failed loading client config")
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return &kubernetes.Clientset{}, errors.New("Failed getting clientset")
+	}
+	return clientset, nil
 }
